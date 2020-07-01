e9b4d614565dcb8ca453a0434ada4eb2904561f6
Akshayus27 <52912821+Akshayus27@users.noreply.github.com>
2020-07-01 09:28:06 +0530

    Add files via upload

---
+++ client/build/precache-manifest.02551dfae702a67184be6dd748ddb7f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14603a608da0f5295a68268d011c438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1a29547d5dd789a8f4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2e1a29547d5dd789a8f4",
+    "url": "/static/js/2.54dd7310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54dd7310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbb8179bedaf5247330",
+    "url": "/static/js/main.be48ce8f.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
